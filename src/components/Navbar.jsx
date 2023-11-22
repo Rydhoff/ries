cf14024399fb202f4cc668956806b6c5c372d93c
Ridho <102776785+Rydhoff@users.noreply.github.com>
2023-11-22 08:35:38 +0700

    Rapiin part 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,30 +14,30 @@ function Navbar({ children }) {
   return (
     <div className="flex">
         <nav className="bg-white shadow-xl w-56 max-sm:hidden h-screen py-6 px-4 flex flex-col rounded-br-3xl rounded-tr-3xl justify-between">
-            <header className="h-40 rounded px-3 py-4">
+            <header className="border-b-2 h-40 rounded px-3 py-4">
                 <img className="h-[60%] outline rounded-full outline-slate-400 p-2 w-auto m-auto" src={ries} alt="ries" />
                 <div className="py-4">
                     <h1 className="text-sm text-slate-600 font-bold">RIES</h1>
                     <h1 className="text-sm text-slate-600 font-semibold">Status : <span className="text-green-400">Connected</span></h1>
                 </div>
             </header>
-            <div className="border-t-2">
-                <div className="flex flex-col space-y-1">
+            <div>
+                <div className="flex flex-col">
                     <label className="px-2 font-bold text-slate-300 text-xs mt-2 mb-1">Dashboard</label>
                     <NavLink to="/" className="font-semibold text-slate-500 aria-[current=page]:text-slate-600 text-sm py-2 px-2 aria-[current=page]:bg-slate-100 rounded">Device</NavLink>
                 </div>
-                <div className="flex flex-col space-y-1">
+                <div className="flex flex-col">
                     <label className="px-2 font-bold text-slate-300 text-xs mt-2 mb-1">Source</label>
                     <NavLink to="/database" className="font-semibold text-slate-500 aria-[current=page]:text-slate-600 text-sm py-2 px-2 aria-[current=page]:bg-slate-100 rounded">Database</NavLink>
                 </div>
-                <div className="flex flex-col space-y-1">
+                <div className="flex flex-col">
                     <label className="px-2 font-bold text-slate-300 text-xs mt-2 mb-1">Message</label>
                     <NavLink to="/autoreply" className="font-semibold text-slate-500 aria-[current=page]:text-slate-600 text-sm py-2 px-2 aria-[current=page]:bg-slate-100 rounded">Auto Reply</NavLink>
                     <NavLink to="/scheduled" className="font-semibold text-slate-500 aria-[current=page]:text-slate-600 text-sm py-2 px-2 aria-[current=page]:bg-slate-100 rounded">Scheduled</NavLink>
                     <NavLink to="/blast" className="font-semibold text-slate-500 aria-[current=page]:text-slate-600 text-sm py-2 px-2 aria-[current=page]:bg-slate-100 rounded">Blast</NavLink>
                     <NavLink to="/send-manual" className="font-semibold text-slate-500 aria-[current=page]:text-slate-600 text-sm py-2 px-2 aria-[current=page]:bg-slate-100 rounded">Send Manual</NavLink>
                 </div>
-                <div className="flex flex-col space-y-1">
+                <div className="flex flex-col">
                     <label className="px-2 font-bold text-slate-300 text-xs mt-2 mb-1">Rest API</label>
                     <NavLink to="/configuration" className="font-semibold text-slate-500 aria-[current=page]:text-slate-600 text-sm py-2 px-2 aria-[current=page]:bg-slate-100 rounded">Configuration</NavLink>
                 </div>
